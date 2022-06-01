0f88aa81673433730283d104a744eb137763e734
Vaern <masondrown@gmail.com>
2022-05-31 19:48:05 -0700

    Moved outputs to CoreCasterRecipes, added radiation values
    I'm gonna have to move this stuff into an int array, and figure out coloring + contrast for the materials. Also, will have to figure out what exactly to do next

--- src/main/java/com/hbm/inventory/recipes/CoreCasterRecipes.java
+++ src/main/java/com/hbm/inventory/recipes/CoreCasterRecipes.java
@@ -1,9 +1,12 @@
 package com.hbm.inventory.recipes;
 
+import com.hbm.hazard.transformer.HazardTransformerRadiationNBT;
 import com.hbm.tileentity.machine.TileEntityMachineCoreCaster;
 import com.hbm.util.Tuple.Pair;
 
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 
 public class CoreCasterRecipes {
 	
@@ -11,6 +14,7 @@ public class CoreCasterRecipes {
 		ItemStack[] consumed = new ItemStack[inputs.length];
 		int quantity = 0;
 		int criticalMass = 0;
+		int radiation = 0;
 		for(byte i = 6; i < 12; i++) {
 			if(inputs[i] == null)
 				continue;
@@ -24,15 +28,28 @@ public class CoreCasterRecipes {
 			if(criticalMass + values[1] * inputs[i].stackSize < criticalLimit) {
 				quantity += values[0] * inputs[i].stackSize;
 				criticalMass += values[1] * inputs[i].stackSize;
+				radiation += values[2] * inputs[i].stackSize;
 				consumed[i] = inputs[i].copy();
 			} else {
-				return new Pair<>(new int[] { 0, 0 }, null);
+				return new Pair<>(new int[] { 0, 0, 0 }, null);
 			}
 		}
 		
-		return new Pair<>(new int[] { quantity, criticalMass }, consumed);
+		return new Pair<>(new int[] { quantity, criticalMass, radiation }, consumed);
 	}
 	
-	
+	public static ItemStack getGunStyleOutput(Item item, int quantity, int criticalMass, int radiation) {
+		if(criticalMass < 25000)
+			return null;
+		
+		ItemStack output = new ItemStack(item);
+		output.stackTagCompound = new NBTTagCompound();
+		output.stackTagCompound.setInteger("quantity", quantity);
+		output.stackTagCompound.setInteger("criticalMass", criticalMass);
+		if(radiation > 0)
+			output.stackTagCompound.setFloat(HazardTransformerRadiationNBT.RAD_KEY, (float)radiation / 1000F);
+		
+		return output;
+	}
 	
 }
