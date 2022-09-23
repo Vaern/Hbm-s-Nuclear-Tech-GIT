ec074126430790570e3a4f274ef36fd6a286f872
Vaern <masondrown@gmail.com>
2022-09-22 18:49:48 -0700

    Leftover TE stuff

--- src/main/java/com/hbm/tileentity/machine/TileEntityMachineBessemer.java
+++ src/main/java/com/hbm/tileentity/machine/TileEntityMachineBessemer.java
@@ -5,9 +5,21 @@ import com.hbm.tileentity.TileEntityMachineBase;
 public class TileEntityMachineBessemer extends TileEntityMachineBase {
 
 	public TileEntityMachineBessemer() {
-		super(0);
+		super(1);
+	}
+
+	@Override
+	public String getName() {
+		return "container.bessemer";
+	}
+
+	@Override
+	public void updateEntity() {
+		
 	}
 	
-	
+	public void networkUnpack() {
+		
+	}
 	
 }
