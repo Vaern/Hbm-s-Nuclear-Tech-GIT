f592bec7f90464f9468bbf52cdb301987707d89e
Vaern <masondrown@gmail.com>
2023-01-25 19:52:54 -0800

    Further further implementation

--- src/main/java/com/hbm/items/special/ItemRelicMod.java
+++ src/main/java/com/hbm/items/special/ItemRelicMod.java
@@ -44,24 +44,22 @@ public class ItemRelicMod extends ItemArmorMod {
 	public Multimap getModifiers(ItemStack armor, ItemStack mod) {
 		Multimap multimap = super.getAttributeModifiers(armor);
 				
-		for(Pair<TraitEnum, Double> trait : getTraitsFromNBT(mod)) {
+		for(Pair<TraitEnum, Double> trait : getTraitsNBT(mod)) {
 			
 		}
 		
 		return multimap;
 	}
 	
-	
-	private ArrayList<Pair<TraitEnum, Double>> getTraitsFromNBT(ItemStack mod) {
+	/* like everything related to implementation of traits, subject to change */
+	private ArrayList<Pair<TraitEnum, Double>> getTraitsNBT(ItemStack mod) {
 		ArrayList traits = new ArrayList<Pair<TraitEnum, Double>>();
 		
-		if(!mod.hasTagCompound())
-			return traits;
+		if(mod == null) return traits; //reconsider
+		if(!mod.hasTagCompound()) return traits;
 		
 		NBTTagCompound tag = mod.stackTagCompound.getCompoundTag("traits");
-		
-		if(tag == null)
-			return traits;
+		if(tag.hasNoTags()) return traits;
 		
 		final int count = tag.getInteger("trait_number");
 		
@@ -73,6 +71,62 @@ public class ItemRelicMod extends ItemArmorMod {
 		}
 		
 		return traits;
+	}
+	
+	//Might need to change this to take the tag directly to prevent redundant checks when generating
+	private void addTraitNBT(ItemStack mod, TraitEnum effect, double magnitude) {
+		if(mod == null) return; //reconsider
+		if(!mod.hasTagCompound()) return;
+		
+		NBTTagCompound tag = mod.stackTagCompound.getCompoundTag("traits");
+		if(tag.hasNoTags()) return;
+		
+		final int count = tag.getInteger("trait_number") + 1;
+		tag.setInteger("trait_number", count);
+		
+		tag.setInteger("t_eff" + count, effect.ordinal());
+		tag.setDouble("t_mag" + count, magnitude);
+		
+		mod.stackTagCompound.setTag("traits", tag);
+	}
+		
+	private void removeTraitsNBT(ItemStack mod, int... remove) {
+		if(mod == null) return; //reconsider
+		if(!mod.hasTagCompound()) return;
+		
+		NBTTagCompound tag = mod.stackTagCompound.getCompoundTag("traits");
+		if(tag.hasNoTags()) return;
+		
+		for(int index : remove) {
+			tag.removeTag("t_eff" + index);
+			tag.removeTag("t_mag" + index);
+		}
+		
+		final int count = tag.getInteger("trait_number");
+		int validIndex = 0; //which index to shift to, or to not shift at all if equal
+		
+		for(int i = 0; i < count; i++) {
+			
+			if(!tag.hasKey("t_eff" + i) || !tag.hasKey("t_mag" + i)) continue;
+			
+			if(validIndex < i) {
+				final int ordinal = tag.getInteger("t_eff" + i);
+				final double magnitude = tag.getDouble("t_mag" + i);
+				
+				tag.setInteger("t_eff" + validIndex, ordinal);
+				tag.setDouble("t_mag" + validIndex, magnitude);
+			}
+			
+			validIndex++;
+			
+			if(i >= count - remove.length) { //get rid of any straggler tags
+				tag.removeTag("t_eff" + i);
+				tag.removeTag("t_mag" + i);
+			}
+		}
+		
+		tag.setInteger("trait_number", count - remove.length); //update amount
+		mod.stackTagCompound.setTag("traits", tag);
 	}
 	
 	public enum StyleEnum {
@@ -100,8 +154,8 @@ public class ItemRelicMod extends ItemArmorMod {
 	
 	@FunctionalInterface
 	private static interface Modifiers {
-		public void getModifiers(Multimap map, double effect);*/
-	}
+		public void getModifiers(Multimap map, double effect);
+	}*/
 	
 	public enum TraitEnum {
 		MAX_HEALTH, //basic sharedmonsterattribute-related traits, proof of concept mostly
