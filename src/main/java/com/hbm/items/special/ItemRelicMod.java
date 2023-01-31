86b639f18420994486fc9cb591a9f322634f07ea
Vaern <masondrown@gmail.com>
2023-01-31 11:15:59 -0800

    Major refactoring
    Much less complicated / more flexible system for storing, reading, and using traits

--- src/main/java/com/hbm/items/special/ItemRelicMod.java
+++ src/main/java/com/hbm/items/special/ItemRelicMod.java
@@ -34,28 +34,35 @@ public class ItemRelicMod extends ItemArmorMod {
 	
 	@Override
 	public void modUpdate(EntityLivingBase entity, ItemStack armor, ItemStack mod) {
+		if(!mod.hasTagCompound()) return;
 		
+		for(NBTTagCompound tag : getTraitsFromNBT(mod.stackTagCompound)) {
+			TraitEnum num = EnumUtil.grabEnumSafely(TraitEnum.class, tag.getInteger(ENUM_KEY));
+			
+			if(num.update != null) num.update.onModUpdate(entity, armor, tag);
+		}
 	}
 	
 	@Override
 	public void modDamage(LivingHurtEvent event, ItemStack armor, ItemStack mod) {
+		if(!mod.hasTagCompound()) return;
 		
+		for(NBTTagCompound tag : getTraitsFromNBT(mod.stackTagCompound)) {
+			TraitEnum num = EnumUtil.grabEnumSafely(TraitEnum.class, tag.getInteger(ENUM_KEY));
+			
+			if(num.damage != null) num.damage.onModDamage(event, armor, tag);
+		}
 	}
 	
 	@Override
 	public Multimap getModifiers(ItemStack armor, ItemStack mod) {
 		Multimap multimap = super.getAttributeModifiers(armor);
-				
-		for(Pair<TraitEnum, Double> trait : getTraitsNBT(mod)) {
-			switch(trait.getKey()) {
-			case SPEED:
-				multimap.put(SharedMonsterAttributes.movementSpeed.getAttributeUnlocalizedName(), //TODO redo
-						new AttributeModifier(ArmorModHandler.UUIDs[((ItemArmor)armor.getItem()).armorType], "NTM Armor Mod Servos", trait.getValue(), 0));
-				break;
+		if(!mod.hasTagCompound()) return multimap;
+		
+		for(NBTTagCompound tag : getTraitsFromNBT(mod.stackTagCompound)) {
+			TraitEnum num = EnumUtil.grabEnumSafely(TraitEnum.class, tag.getInteger(ENUM_KEY));
 			
-			
-			default:
-			}
+			if(num.mod != null) num.mod.getModifiers(multimap, armor, tag);
 		}
 		
 		return multimap;
@@ -66,19 +73,126 @@ public class ItemRelicMod extends ItemArmorMod {
 		ItemStack stack = new ItemStack(item);
 		stack.stackTagCompound = new NBTTagCompound();
 		
-		final double mag = 5;//rand.nextDouble() * 4;
-		addTraitNBT(stack, TraitEnum.SPEED, mag);
-		System.out.println(mag);
+		NBTTagCompound[] tags = new NBTTagCompound[3];
 		
+		for(int i = 0; i < 3; i++) {
+			tags[i] = new NBTTagCompound();
+			
+			final TraitEnum num = EnumUtil.grabEnumSafely(TraitEnum.class, rand.nextInt());
+			final double mag = rand.nextDouble() * 2;
+			final int mode = Math.abs(rand.nextInt() % 3);
+			System.out.println(num.name() + ", Magnitude: " + mag + ", Mode: " + mode);
+			
+			tags[i].setInteger(ENUM_KEY, num.ordinal());
+			tags[i].setDouble("effect", mag);
+			tags[i].setInteger("mode", mode);
+		}
+		
+		addTraitsToNBT(stack.stackTagCompound, tags);
 		return stack;
 	}
 	
-	private static final String T_COMPOUND_KEY = "traits";
+	/*private static final String T_COMPOUND_KEY = "traits";
 	private static final String T_EFFECT_KEY = "t_eff_";
-	private static final String T_MAGNITUDE_KEY = "t_mag_";
+	private static final String T_MAGNITUDE_KEY = "t_mag_";*/
+	
+	/** balls */
+	private static final String TRAIT_KEY = "trait_list";
+	private static final String ENUM_KEY = "enum_ordinal";
+	
+	private static ArrayList<NBTTagCompound> getTraitsFromNBT(NBTTagCompound nbt) {
+		if(nbt == null) return new ArrayList();
+		
+		NBTTagList tagList = nbt.getTagList(TRAIT_KEY, 10); //taglist of tagcompounds
+		ArrayList<NBTTagCompound> list = new ArrayList<NBTTagCompound>();
+		
+		for(int i = 0; i < tagList.tagCount(); i++) {
+			NBTTagCompound tag = tagList.getCompoundTagAt(i);
+			if(!tag.hasNoTags()) list.add(tag);
+		}
+		
+		return list;
+	}
+	
+	private static void addTraitsToNBT(NBTTagCompound nbt, NBTTagCompound...traits) {
+		if(nbt == null || traits == null) return;
+		
+		NBTTagList tagList = nbt.getTagList(TRAIT_KEY, 10);
+		
+		for(int i = 0; i < traits.length; i++) {
+			if(!traits[i].hasNoTags()) tagList.appendTag(traits[i]);
+		}
+		
+		nbt.setTag(TRAIT_KEY, tagList);
+	}
+	
+	private static void removeTraitsFromNBT(NBTTagCompound nbt, int...indices) {
+		if(nbt == null || indices == null) return;
+		
+		NBTTagList tagList = nbt.getTagList(TRAIT_KEY, 10);
+		
+		for(int i = 0; i < indices.length; i++) {
+			tagList.removeTag(i);
+		}
+		
+		nbt.setTag(TRAIT_KEY, tagList);
+	}
+	
+	public static enum StyleEnum {
+		MAGICAL,
+		MYSTICAL,
+		MECHANICAL,
+		CYBERNETIC;
+	}
+	
+	public static enum TraitEnum {
+		//TODO: create a manager / handler for this instead of stuffing it all into here. Could work in/with ArmorModHandler?
+		//basic sharedmonsterattribute-related traits, proof of concept mostly
+		MAX_HEALTH((map, a, tag) -> map.put(SharedMonsterAttributes.maxHealth.getAttributeUnlocalizedName(),
+				new AttributeModifier(ArmorModHandler.UUIDs[((ItemArmor)a.getItem()).armorType],
+				"NTM Armor Mod Health", tag.getDouble("effect"), tag.getInteger("mode")))),
+		KNOCKBACK((map, a, tag) -> map.put(SharedMonsterAttributes.knockbackResistance.getAttributeUnlocalizedName(),
+				new AttributeModifier(ArmorModHandler.UUIDs[((ItemArmor)a.getItem()).armorType],
+				"NTM Armor Mod Knockback", tag.getDouble("effect"), tag.getInteger("mode")))),
+		SPEED((map, a, tag) -> map.put(SharedMonsterAttributes.movementSpeed.getAttributeUnlocalizedName(),
+				new AttributeModifier(ArmorModHandler.UUIDs[((ItemArmor)a.getItem()).armorType],
+				"NTM Armor Mod Speed", tag.getDouble("effect"), tag.getInteger("mode")))),
+		DAMAGE((map, a, tag) -> map.put(SharedMonsterAttributes.attackDamage.getAttributeUnlocalizedName(),
+				new AttributeModifier(ArmorModHandler.UUIDs[((ItemArmor)a.getItem()).armorType],
+				"NTM Armor Mod Damage", tag.getDouble("effect"), tag.getInteger("mode"))));
+		
+		protected ModUpdate update;
+		protected ModDamage damage;
+		protected Modifiers mod;
+		
+		private TraitEnum(ModUpdate update, ModDamage damage, Modifiers mod) {
+			this.update = update;
+			this.damage = damage;
+			this.mod = mod;
+		}
+		
+		private TraitEnum(Modifiers mod) { this(null, null, mod); }
+	}
+	
+	@FunctionalInterface
+	private static interface ModUpdate {
+		public void onModUpdate(EntityLivingBase entity, ItemStack armor, NBTTagCompound tag);
+	}
+	
+	@FunctionalInterface
+	private static interface ModDamage {
+		public void onModDamage(LivingHurtEvent event, ItemStack armor, NBTTagCompound tag);
+	}
+	
+	@FunctionalInterface
+	private static interface Modifiers {
+		public void getModifiers(Multimap map, ItemStack armor, NBTTagCompound tag);
+	}
+	
+	/** balls*/
 	
 	/* like everything related to implementation of traits, subject to change */
-	private static ArrayList<Pair<TraitEnum, Double>> getTraitsNBT(ItemStack mod) {
+	/*private static ArrayList<Pair<TraitEnum, Double>> getTraitsNBT(ItemStack mod) {
 		ArrayList traits = new ArrayList<Pair<TraitEnum, Double>>();
 		
 		if(mod == null) return traits; //reconsider
@@ -114,7 +228,7 @@ public class ItemRelicMod extends ItemArmorMod {
 		
 		mod.stackTagCompound.setTag(T_COMPOUND_KEY, tag);
 	}
-		
+	
 	private static void removeTraitsNBT(ItemStack mod, int... remove) {
 		if(mod == null) return; //reconsider
 		if(!mod.hasTagCompound()) return;
@@ -152,16 +266,9 @@ public class ItemRelicMod extends ItemArmorMod {
 		
 		tag.setInteger("trait_number", count - remove.length); //update amount
 		mod.stackTagCompound.setTag(T_COMPOUND_KEY, tag);
-	}
+	}*/
 	
-	public static enum StyleEnum {
-		MAGICAL,
-		MYSTICAL,
-		MECHANICAL,
-		CYBERNETIC
-	}
-	
-	private static final String STYLE_KEY = "style_";
+	/*private static final String STYLE_KEY = "style_";
 	
 	//0 for primary, 1 for secondary
 	private static void setStyleNBT(ItemStack mod, StyleEnum num, int priority) {
@@ -183,14 +290,7 @@ public class ItemRelicMod extends ItemArmorMod {
 		}
 		
 		return styles;
-	}
-	
-	public static enum TraitEnum {
-		MAX_HEALTH, //basic sharedmonsterattribute-related traits, proof of concept mostly
-		KNOCKBACK,
-		SPEED,
-		DAMAGE
-	}
+	}*/
 	
 	//might actually be optimal to make a full 32 (4 * 8) UUID-strong thing for modifiers. hell? yes. flexible? also yes.
 	
